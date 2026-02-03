24a5ab1477222792823471c9ef10b95bf56a9986
tianeyy <tian.garcia395@gmail.com>
2026-02-03 23:13:35 +0800

    refactor: rename auth

--- src/features/authentication/auth-controller.ts
+++ src/features/authentication/auth-controller.ts
@@ -1,34 +1,16 @@
 import type { FastifyInstance } from 'fastify';
 import { match } from 'ts-pattern';
-import { createUser } from './create-user';
+import { registerUser } from './register-user';
 
 export default async function userController(fastify: FastifyInstance) {
-  fastify.route<{ Body: { googleId: string; name: string; email: string; avatarUrl?: string | null } }>({
+  fastify.route<{
+    Body: { googleId: string; name: string; email: string; avatarUrl?: string | null; password: string };
+  }>({
     method: 'POST',
     url: '/api/v1/users',
     schema: {
       summary: 'Create a new user',
       tags: ['users'],
-      body: {
-        type: 'object',
-        required: ['googleId', 'name', 'email'],
-        properties: {
-          googleId: { type: 'string', description: 'Google ID of the user', example: '1234567890' },
-          name: { type: 'string', description: 'Name of the user', example: 'John Doe' },
-          email: {
-            type: 'string',
-            format: 'email',
-            description: 'Email address of the user',
-            example: 'john.doe@example.com',
-          },
-          avatarUrl: {
-            type: 'string',
-            format: 'uri',
-            description: 'Avatar URL of the user',
-            example: 'https://example.com/avatar.jpg',
-          },
-        },
-      },
       response: {
         201: {
           description: 'Shortened URL created',
@@ -44,10 +26,11 @@ export default async function userController(fastify: FastifyInstance) {
       },
     },
     handler: async (request, reply) => {
-      const result = await createUser(
+      const result = await registerUser(
         {
           googleId: request.body.googleId,
           name: request.body.name,
+          password: request.body.password,
           email: request.body.email,
           avatarUrl: request.body.avatarUrl ?? null,
         },
--- src/features/authentication/register-user.ts
+++ src/features/authentication/register-user.ts
@@ -1,10 +1,12 @@
 import { User } from '@domain/entities';
+import { passwordCrypto } from '@infrastructure/crypto/password';
 import type { UseCaseDependencies } from '@infrastructure/di';
 import { z } from 'zod';
 
 const createUserParamsSchema = z.object({
-  googleId: z.string().min(1),
+  googleId: z.string().min(1).nullable().default(null),
   name: z.string().min(1),
+  password: z.string().min(6),
   email: z.email(),
   avatarUrl: z.string().nullable().default(null),
 });
@@ -12,7 +14,7 @@ const createUserParamsSchema = z.object({
 export type CreateUserParams = z.input<typeof createUserParamsSchema>;
 export type CreateUserResult = { type: 'success'; id: string } | { type: 'error' };
 
-export async function createUser(
+export async function registerUser(
   params: CreateUserParams,
   { logger, repositories }: UseCaseDependencies,
 ): Promise<CreateUserResult> {
@@ -20,11 +22,14 @@ export async function createUser(
 
   const validated = createUserParamsSchema.parse(params);
 
+  const hashedPassword = await passwordCrypto.hash(validated.password);
+
   const user = new User({
     id: crypto.randomUUID(),
     googleId: validated.googleId,
     name: validated.name,
     email: validated.email,
+    password: hashedPassword,
     avatarUrl: validated.avatarUrl,
   });
 
